0830c9f6531a2aa7e7f8e4fba914bf65f9700e63
G4tLan <langagift01@gmail.com>
2020-02-19 22:42:11 +0200

    first commit

---
+++ Assets/Scripts/FloorManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+
+    public GameObject[] floors;
+    public float floorLength = 30.0f;
+    public int numOfFloors = 7;
+
+    public static FloorManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Pool()
+    {
+        Debug.Log("pool");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnMany(numOfFloors);
+    }
+    
+    void Spawn(float offset)
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y = 0.0f;
+        newPosition.x = 0.0f;
+        newPosition.z += floorLength* offset - floorLength/2.0f;
+        int index = Random.Range(0, floors.Length - 1);
+        Instantiate(floors[index], newPosition, floors[index].transform.rotation);
+    }
+
+    void SpawnMany(int num)
+    {
+        for(int i = 0; i < numOfFloors; i++)
+        {
+            Spawn(i);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public float maxFowardPosition = 100.0f;
+    public Transform MovementController;
+
+    float prevForwardPosition;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        prevForwardPosition = MovementController.position.z;
+    }
+
+    void EndOfSegmentTrigger()
+    {
+        if(MovementController.position.z - prevForwardPosition >= FloorManager.Instance.floorLength)
+        {
+            FloorManager.Instance.Pool();
+            prevForwardPosition += FloorManager.Instance.floorLength;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EndOfSegmentTrigger();
+    }
+}
