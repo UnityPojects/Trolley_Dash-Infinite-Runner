b9bc65867bc4e539079d09c036fbbe8c25fa70ed
GiftLanga <glanga@retrorabbit.co.za>
2020-02-26 20:36:50 +0200

    basic pickup

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pickable")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
